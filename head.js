cb33c92b14f53cbbd445e2e1803d04bc7ee295eb
kir- <graysonclee@gmail.com>
2019-06-25 00:05:19 +0000

    head.js completed

---
+++ head.js
@@ -0,0 +1,14 @@
+const head = function(array1) {
+  return (array1[0]);
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), undefined);
