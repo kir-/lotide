2e9e7dbf14f40653a5b4c523a30607ba8c1bd9c2
kir- <graysonclee@gmail.com>
2019-06-25 19:08:34 +0000

    finished array compare

---
+++ eqArrays.js
@@ -0,0 +1,14 @@
+const eqArrays = function(actual, expected) {
+  for (let x = 0; x < actual.length; x++) {
+    if (actual[x] !== expected[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
