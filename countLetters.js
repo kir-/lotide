4b2358d6c65601ed6370ad2d35ee6ab92a12e7b0
kir- <graysonclee@gmail.com>
2019-06-26 02:03:41 +0000

    countLetters completed

---
+++ countLetters.js
@@ -0,0 +1,12 @@
+const countLetters = (sentance) => {
+  let letterHolder = {};
+  for (let x of sentance) {
+    //if (x.match(/[a-zA-Z]/)) {
+    if (x !== " ") {
+      x in letterHolder ? letterHolder[x]++ : letterHolder[x] = 1;
+    }
+  }
+  return letterHolder;
+};
+
+console.log(countLetters("hello my name is"));
